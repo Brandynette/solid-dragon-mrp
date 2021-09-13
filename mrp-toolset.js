edee79c1cf8f45bf28d58dfed46a083b1b7e5468
Brandynette <85776993+Brandynette@users.noreply.github.com>
2021-09-13 04:59:08 +0200

    enviroment ini
    hell im done

---
+++ mrp-toolset.js
@@ -0,0 +1,27 @@
+//Morphine token ethereum smart contracts using solidity
+//yes im sure i can do it && then learning to do it are 2 things
+//why o why... Imdipuuuu
+function make(manual){if noob("CRAP").train};
+
+
+//worst hello world evaaa
+//https://ethereum.org/en/developers/tutorials/hello-world-smart-contract
+
+//https://ethereum.org/en/developers/docs/oracles/
+//https://dapp.tools/
+
+//stand alone Dev network
+
+geth --datadir ./.ethereum/devnet --dev console
+//runs? then:
+personal.newAccount() =>
+
+address=0xb35daFEc3aC5b440fc63725B74FE4F2078932cB7
+path=F:\geth\.ethereum\devnet\keystore\UTC--2021-09-13T02-44-49.017123300Z--b35dafec3ac5b440fc63725b74fe4f2078932cb7
+pass="mrp"
+
+//After inputting a passphrase you should see your account address. Type ‘exit,’ hit the return key, and enter the command to start mining:
+geth --datadir ./.ethereum/devnet --dev --mine --minerthreads 1 --etherbase 0
+
+//https://solidity-by-example.org/hello-world
+geth console
